a686dca563106be5ac01bc75461685bd67e7b17b
maxmuv <mmaksimenko534@gmail.com>
2023-03-27 02:36:38 +0300

    implemented category_view

--- bookkeeper/view/category_view.py
+++ bookkeeper/view/category_view.py
@@ -1,4 +1,5 @@
-from PySide6 import QtWidgets
+from PySide6 import QtWidgets, QtCore, QtGui
+from typing import Callable
 
 
 class CategoryView(QtWidgets.QWidget):
@@ -8,6 +9,9 @@ class CategoryView(QtWidgets.QWidget):
         self.sum_label = QtWidgets.QLabel("Сумма")
         self.vbox.addWidget(self.sum_label, 0, 0)
         self.line = QtWidgets.QLineEdit()
+        only_int = QtGui.QIntValidator()
+        only_int.setRange(0, 999999)
+        self.line.setValidator(only_int)
         self.vbox.addWidget(self.line, 0, 1)
         self.cat_label = QtWidgets.QLabel("Категории")
         self.vbox.addWidget(self.cat_label, 1, 0)
@@ -23,10 +27,43 @@ class CategoryView(QtWidgets.QWidget):
         self.dlg.setWindowTitle("Категории")
         self.dlg.resize(300, 300)
         self.dlg_vbox = QtWidgets.QVBoxLayout()
-        self.tree_view = QtWidgets.QTreeView()
+        self.tree_view = QtWidgets.QTreeWidget()
         self.dlg_vbox.addWidget(self.tree_view)
         self.dlg.setLayout(self.dlg_vbox)
+        self.current_item = ""
 
     def edit_button_clicked(self):
         self.dlg.exec()
 
+    def set_data(self, cat: list[tuple[str, str | None]]) -> None:
+        self.tree_view.invisibleRootItem().setFlags(QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsEditable)
+        self.tree_view.invisibleRootItem().setExpanded(True)
+        cat_list = []
+        node_list = {}
+        for c_tuple in cat:
+            child = QtWidgets.QTreeWidgetItem()
+            child.setText(0, c_tuple[0])
+            child.setExpanded(True)
+            child.setFlags(QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsEditable)
+            node_list[c_tuple[0]] = child
+            cat_list.append(c_tuple[0])
+            if c_tuple[1] is None:
+                self.tree_view.invisibleRootItem().addChild(child)
+            else:
+                node_list[c_tuple[1]].addChild(child)
+        self.list_view.addItems(cat_list)
+
+    def register_handler(self, handler: Callable[[str, str], None]):
+        def act_handler(item):
+            self.current_item = item.text(0)
+            print(self.current_item)
+
+        def change_handler(item):
+            handler(self.current_item, item.text(0))
+        self.tree_view.itemDoubleClicked.connect(act_handler)
+        self.tree_view.itemChanged.connect(change_handler)
+
+    def adder_handler(self, handler: Callable[[int, str],None]):
+        def func():
+            handler(int(self.line.text()), self.list_view.currentText())
+        self.sum_button.clicked.connect(func)
--- bookkeeper/view/view.py
+++ bookkeeper/view/view.py
@@ -29,8 +29,8 @@ class View(AbstractView):
         self.vbox.addWidget(self.category_view)
         self.window.setLayout(self.vbox)
 
-    def set_category_list(self, cat_list: list[str]) -> None:
-        pass
+    def set_category_list(self, cat_list: list[tuple[str, str | None]]) -> None:
+        self.category_view.set_data(cat_list)
 
     def set_budget(self, budget: list[list[int]]) -> None:
         self.budget_view.set_data(budget)
@@ -38,11 +38,14 @@ class View(AbstractView):
     def set_expense_list(self, exp_list: list[list[str]]) -> None:
         self.expense_view.set_data(exp_list)
 
-    def register_cat_modifier(self, handler: Callable[[int], None]):
-        pass
+    def register_cat_modifier(self, handler: Callable[[str, str], None]):
+        self.category_view.register_handler(handler)
 
     def register_exp_modifier(self, handler: Callable[[int, int, str], None]):
         self.expense_view.set_handler(handler)
 
     def register_budget_modifier(self, handler: Callable[[int, int, str], None]):
         self.budget_view.register_handler(handler)
+
+    def register_adder_handler(self, handler: Callable[[int, str], None]):
+        self.category_view.adder_handler(handler)
